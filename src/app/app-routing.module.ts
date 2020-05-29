f2a527790a0a2cd1e01ab95bd729b833a0cf58ff
kennedysean <kenne476@msu.edu>
2020-05-29 11:54:52 -0400

    F001: Routing for initial five pages is configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { EducationComponent } from './pages/education/education.component';
+import { EventsComponent } from './pages/events/events.component';
+import { GetInvolvedComponent } from './pages/get-involved/get-involved.component';
 
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-us', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'get-involved', component: GetInvolvedComponent },
+  // wildcard route
+  { path: "**", redirectTo: '' },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
