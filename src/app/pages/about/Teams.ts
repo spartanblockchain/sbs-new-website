bc9d47964edc8c8cb2a438d862586df37db91887
Sawyer Ruben <sawyer.ruben@gmail.com>
2020-06-01 11:58:37 -0400

    Grid layout for team members

---
+++ src/app/pages/about/Teams.ts
@@ -0,0 +1,88 @@
+export const sbsTeamMembers = [
+  {fname: "Sean", lname: "Kennedy",
+   position: "President",
+   image: "seanKennedy.jpg", link: "https://www.linkedin.com/in/sean-b-kennedy/"
+  },
+  {fname: "Tyler", lname: "Weisner",
+    position: "VP of Technology",
+    image: "empty.png", link: "https://www.linkedin.com/in/tyler-weisner-a7a65715b/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  }
+
+]
+
+export const boardOfDirectors = [
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  }
+]
+
+export const professionalAdvisors = [
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  }
+]
+
+export const facultyAdvisors = [
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  },
+  {fname: "Maggie", lname: "Francis",
+    position: "VP of Diversity and Inclusion",
+    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  }
+]
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { sbsTeamMembers, boardOfDirectors, facultyAdvisors, professionalAdvisors } from './Teams'
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  sbsTeamMembers = sbsTeamMembers;
+  boardOfDirectors = boardOfDirectors;
+  facultyAdvisors = facultyAdvisors;
+  professionalAdvisors = professionalAdvisors;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
