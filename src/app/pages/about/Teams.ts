193a5ff27f64bb980db91daef8561558dc37db02
Sawyer Ruben <sawyer.ruben@gmail.com>
2020-06-02 08:45:39 -0400

    removing strange whitespace from grids

--- src/app/pages/about/Teams.ts
+++ src/app/pages/about/Teams.ts
@@ -11,20 +11,20 @@ export const sbsTeamMembers = [
     position: "VP of Diversity and Inclusion",
     image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
+  {fname: "Syed", lname: "Zaidi",
+    position: "VP of Marketing",
     image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
+  {fname: "Sawyer", lname: "Ruben",
+    position: "VP of Business Development",
     image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
+  {fname: "Chris", lname: "Nosowsky",
+    position: "Lead Developer",
     image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
+  {fname: "Parker", lname: "Nolan",
+    position: "Project Manager",
     image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
   },
   {fname: "Maggie", lname: "Francis",
@@ -63,54 +63,54 @@ export const sbsTeamMembers = [
 ]
 
 export const boardOfDirectors = [
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Siddhant", lname: "Chhabra",
+    position: "Bytelion",
+    image: "empty.png", link: "https://www.linkedin.com/in/siddhant-chhabra/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Andrew", lname: "Mardquart",
+    position: "Accenture",
+    image: "empty.png", link: "https://www.linkedin.com/in/andrewgmarquardt/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Sean", lname: "Kennedy",
+    position: "MSU Master's of Computer Science",
+    image: "empty.png", link: "https://www.linkedin.com/in/sean-kennedy-096ba616b/"
   }
 ]
 
 export const professionalAdvisors = [
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Rohun", lname: "Jauhar",
+    position: "Jauhar Capital",
+    image: "empty.png", link: "https://www.linkedin.com/in/rohunjauhar"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Jeff", lname: "Rayis",
+    position: "Michigan State University",
+    image: "empty.png", link: "https://www.linkedin.com/in/jeffrayis"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Jeff", lname: "Wesley",
+    position: "Red Cedar Ventures",
+    image: "empty.png", link: "https://www.linkedin.com/in/jeff-wesley-2026549/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "David", lname: "Smith",
+    position: "Bitcoin Bulls",
+    image: "empty.png", link: "https://www.linkedin.com/in/davidssmith/"
   }
 ]
 
 export const facultyAdvisors = [
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Neil", lname: "Kane",
+    position: "Director of Undergraduate Entrepreneurship",
+    image: "empty.png", link: "https://www.linkedin.com/in/neilkane"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Carla", lname: "Reyes",
+    position: "Assistant Professor of Law & Director of Legal RnD",
+    image: "empty.png", link: "https://www.linkedin.com/in/carla-reyes-67a90181"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Joseph", lname: "Joyce",
+    position: "Assistant Professor of Supply Chain Management ",
+    image: "empty.png", link: "https://www.linkedin.com/in/joseph-joyce-ph-d-1803a1/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "VP of Diversity and Inclusion",
-    image: "empty.png", link: "https://www.linkedin.com/in/maggie-francis/"
+  {fname: "Aubrey", lname: "Wigner",
+    position: "Assistant Professor of Entrepreneurship & Innovation",
+    image: "empty.png", link: "https://www.linkedin.com/in/aubrey-wigner-2300459a/"
   }
 ]
