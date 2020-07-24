a35c90c0ed5964416f289a10157aaeff9213fd14
sawyerruben <sawyer.ruben@gmail.com>
2020-07-24 14:14:36 -0400

    adding images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import {NgEmbedMediumModule} from 'ng-embed-medium';
-// import {NgMediumModule} from 'ng-medium'
+import {NgMediumModule} from 'ng-medium'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
