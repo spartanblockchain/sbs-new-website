2d928de25aa23f7887de227574acb0e9a037c6dd
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-22 16:19:08 -0400

    add server info to envrionment variables instead of hard coded into app. ng build --prod for docs folder

--- src/app/pages/get-involved/services/contact-form.service.ts
+++ src/app/pages/get-involved/services/contact-form.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactFormService {
-  constructor(private http: Http) {}
+
+  environmentUrl : string = '';
+
+  constructor(private http: Http) {
+    this.environmentUrl = environment.apiUrl
+  }
 
   sendEmail(body) {
-    return this.http.post('https://spartan-blockchain-contact.herokuapp.com/sendEmail', body); // Heroku API Endpoint
+    return this.http.post(this.environmentUrl + '/sendEmail', body); // Heroku API Endpoint
   }
 }
