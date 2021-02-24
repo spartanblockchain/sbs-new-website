f60ddeb02860424700ef9290128a5e338f9e44c4
Tyler Weisner <weisnert@cse.msu.edu>
2021-02-23 19:24:36 -0500

    Added 404.html to angular.json to be included in build and allow refresh on all pages

--- docs/polyfills-es2015.js
+++ docs/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! E:\Racec\Documents\SpartanBlockchain\sbssite\sbs-website\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/tylerweisner/Desktop/SBS📗/sbs-website/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
