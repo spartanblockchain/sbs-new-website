ba5aad31cae5ab6360918fc4a1c6ff328ca93bfb
jlago <lagorege@msu.edu>
2021-12-28 16:02:41 -0500

    Fall 2021 Team Update

--- src/app/pages/about/Teams.ts
+++ src/app/pages/about/Teams.ts
@@ -1,106 +1,73 @@
 export const sbsTeamMembers = [
-  {fname: "Sean", lname: "Kennedy",
-   position: "President",
-   image: "seanKennedy.jpg", link: "https://www.linkedin.com/in/sean-b-kennedy/"
+  {fname: "Jerry", lname: "LaGore",
+    position: "President",
+    image: "jerryLaGore.JPG", link: "https://www.linkedin.com/in/jerrylagore/"
   },
-  {fname: "Maggie", lname: "Francis",
-    position: "Vice President of Diversity and Inclusion",
-    image: "maggie.jpg", link: "https://www.linkedin.com/in/maggie-francis/"
-  },
-  {fname: "Tyler", lname: "Weisner",
+  {fname: "Sungyu", lname: "Kwon",
     position: "Vice President of Technology",
-    image: "tylerWeisner.jpg", link: "https://www.linkedin.com/in/tyler-weisner/"
+    image: "sungyuKwon.jpg", link: "https://www.linkedin.com/in/sungyu-kwon-8a0271169/"
   },
-  {fname: "Syed", lname: "Zaidi",
-    position: "Vice President of Marketing",
-    image: "syed.jpg", link: "https://www.linkedin.com/in/syed-akbar-zaidi/"
+  {fname: "Nikita", lname: "Gupta",
+    position: "Vice President of Research",
+    image: "nikitaGupta.jpg", link: "https://www.linkedin.com/in/nikitagupta2000/"
   },
-  {fname: "Sawyer", lname: "Ruben",
+  {fname: "Kiinga", lname: "Kioi",
     position: "Vice President of Business Development",
-    image: "sawyerRuben.jpg", link: "https://www.sawyerru.github.io/resume/"
+    image: "kiinga.jpg", link: "https://www.linkedin.com/in/kiinga-kioi-18064816a/"
   },
-  {fname: "Chris", lname: "Nosowsky",
-    position: "Lead Developer",
-    image: "chris.jpg", link: "https://www.linkedin.com/in/christopher-nosowsky/"
-  },
-  {fname: "Parker", lname: "Nolan",
-    position: "Project Manager",
-    image: "parker.png", link: "https://www.linkedin.com/in/parkernolan/"
-  },
-  {fname: "Anna", lname: "De Biasi",
-    position: "Senior Developer",
-    image: "anna.jpg", link: "https://www.linkedin.com/in/anna-debiasi/"
-  },
-  {fname: "Abhi", lname: "Vyas",
-    position: "Technical Analyst",
-    image: "abhi.png", link: "https://www.linkedin.com/in/abhijit-vyas-706784141/"
-  },
-  {fname: "Drew", lname: "Goretzka",
-    position: "Contributing Writer",
-    image: "drew.png", link: "https://www.linkedin.com/in/drewgoretzka/"
+  {fname: "Ashu", lname: "Acharya",
+    position: "Vice President of Outreach",
+    image: "ashu.PNG", link: "https://www.linkedin.com/in/ashuacharya/"
   },
   {fname: "Advik", lname: "Agrawal",
     position: "Business Analyst",
     image: "advik.JPG", link: "https://www.linkedin.com/in/advikag/"
   },
-  {fname: "Neil", lname: "Potdukhe",
-    position: "Technical Analyst",
-    image: "neil-p.JPG", link: "https://www.linkedin.com/in/neil-p-039814122/"
-  },
   {fname: "Ian", lname: "Berriel",
     position: "Technical Analyst",
-    image: "ian.jpg", link: "https://www.linkedin.com/in/maggie-francis/"
-  },
-  {fname: "Kiinga", lname: "Kioi",
-    position: "Technical Analyst",
-    image: "kiinga.jpg", link: "https://www.linkedin.com/in/kiinga-kioi-18064816a/"
-  },
-  {fname: "Jerry", lname: "LaGore",
-    position: "Technical Analyst",
-    image: "jerry.jpg", link: "https://www.linkedin.com/in/jerrylagore/"
-  },
-  {fname: "Zachariah", lname: "Dittenbir",
-    position: "Business Analyst",
-    image: "zach.JPG", link: "https://www.linkedin.com/in/zachariah-dittenbir-853a72173/"
-  },
-  {fname: "Olivia", lname: "Simone",
-    position: "Executive Editor of Marketing",
-    image: "olivia.jpg", link: "https://www.linkedin.com/in/olivia-simone-993229190/"
+    image: "ian.jpg", link: "https://www.linkedin.com/in/ianberriel/"
   },
   {fname: "Arriyan", lname: "Raza",
     position: "Business Analyst",
     image: "arriyan.JPG", link: "https://www.linkedin.com/in/raza-msu/"
   },
-  {fname: "Ashu", lname: "Acharya",
-    position: "Technical Analyst",
-    image: "ashu.PNG", link: "https://www.linkedin.com/in/ashu-acharya-5314ba166"
-  },
-  {fname: "Mateo", lname: "Marchan",
-    position: "Business Analyst",
-    image: "mateo.jpg", link: "https://www.linkedin.com/in/mateomarchan/"
-  },
-  {fname: "Nikita", lname: "Gupta",
-    position: "Technical Analyst",
-    image: "nikita.png", link: "https://www.linkedin.com/in/nikitagupta2000/"
-  },
-  {fname: "Sungyu", lname: "Kwon",
-    position: "Technical Analyst",
-    image: "sungyu.jpg", link: "https://www.linkedin.com/in/sungyu-kwon-8a0271169/"
-  },
   {fname: "Freddy", lname: "Merlin",
     position: "Technical Analyst",
     image: "freddy.png", link: "https://www.linkedin.com/in/freddymerlin/"
   },
-  {fname: "Austin", lname: "Sobie",
+  {fname: "Brian", lname: "George",
     position: "Business Analyst",
-    image: "", link: "https://www.linkedin.com/in/austin-sobie-57809264/"
+    image: "brianGeorge.jpg", link: "https://www.linkedin.com/in/briangeorge-123/"
+  },
+  {fname: "Jaspreet", lname: "Singh",
+    position: "Business Analyst",
+    image: "jaspreetSingh.png", link: "https://www.linkedin.com/in/jaspreet-singh-msu/"
+  },
+  {fname: "Max", lname: "Goldstein",
+    position: "Business Analyst",
+    image: "maxGoldstein.JPG", link: "https://www.linkedin.com/in/jaspreet-singh-msu/"
+  },
+  {fname: "Nate", lname: "Jefferson",
+    position: "Business Analyst",
+    image: "nateJefferson.jpg", link: "https://www.linkedin.com/in/nathanojefferson/"
+  },
+  {fname: "Nate", lname: "Mikkola",
+    position: "Technical Analyst",
+    image: "nateMikkola.jpg", link: "https://www.linkedin.com/in/nate-mikkola-0b9671228/"
+  },
+  {fname: "Riley", lname: "Cook",
+    position: "Technical Analyst",
+    image: "rileyCook.jpg", link: "https://www.linkedin.com/in/riley-cook-0a0a7819a/"
+  },
+  {fname: "Tej", lname: "Sutariya",
+    position: "Business Analyst",
+    image: "tejSutariya.jpg", link: "https://www.linkedin.com/in/tejsutariya/"
   }
-
 ]
 
 export const boardOfDirectors = [
   {fname: "Siddhant", lname: "Chhabra",
-    position: "Bytelion",
+    position: "TEDCO",
     image: "sidd.jpg", link: "https://www.linkedin.com/in/siddhant-chhabra/"
   },
   {fname: "Andrew", lname: "Marquardt",
@@ -108,8 +75,8 @@ export const boardOfDirectors = [
     image: "andrew.png", link: "https://www.linkedin.com/in/andrewgmarquardt/"
   },
   {fname: "Sean", lname: "Kennedy",
-    position: "MSU Master's of Computer Science",
-    image: "seanKennedy.jpg", link: "https://www.linkedin.com/in/sean-kennedy-096ba616b/"
+    position: "Omniex",
+    image: "seanKennedy.jpg", link: "https://www.linkedin.com/in/sean-b-kennedy/"
   }
 ]
 
@@ -133,9 +100,9 @@ export const professionalAdvisors = [
 ]
 
 export const facultyAdvisors = [
-  {fname: "Neil", lname: "Kane",
-    position: "Director of Undergraduate Entrepreneurship",
-   image: "neil.jpg", link: "https://www.linkedin.com/in/neilkane"
+  {fname: "Tom", lname: "Holt",
+    position: "Director of School of Criminal Justice",
+   image: "tomHolt.jpg", link: "https://www.linkedin.com/in/tom-holt-3242a322/"
   },
   {fname: "Aubrey", lname: "Wigner",
     position: "Assistant Professor of Entrepreneurship & Innovation",
